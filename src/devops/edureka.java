b7243072ae53adf8a9d5ff52dff15ad1316e3952
dharshinimsb <dharshinimsb@gmail.com>
2025-04-10 23:14:41 +0530

    1st Branch changes

--- src/devops/edureka.java
+++ src/devops/edureka.java
@@ -1,10 +1,22 @@
 package devops;
 
+
 public class edureka {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World");
-
+		edureka m = new edureka();
+		m.MagicLand();
+		m.Stars();
+	}
+	public void MagicLand()
+	{
+		System.out.println("God Bless Both of You...!!!");
+	}
+	
+	public void Stars()
+	{
+		System.out.println("Love My Unniverse...!!!");
 	}
 
 }
